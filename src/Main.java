4df45bd639fef1221e9dc7e3a97bb58f59656b45
Yusufdmrc <yusuffdemircii@outlook.com>
2021-12-19 12:47:02 +0300

    Çin Zodyağı Hesaplama

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int yil;
+
+        Scanner inp=new Scanner(System.in);
+
+        System.out.println("Doğum yılınızı giriniz");
+        yil=inp.nextInt();
+
+        switch (yil%12){
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maynun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+
+        }
+
+    }
+}
